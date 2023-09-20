778ddf92fa45d091b4f358efbb8b2d903bfc6cba
Rikard Dotzler <rikardnoer@gmail.com>
2023-09-20 20:45:41 +0200

    Remove unused routes

--- src/route/user-routes.ts
+++ src/route/user-routes.ts
@@ -6,14 +6,6 @@ import { randomInt } from "crypto";
 export const register = async (server: Server, userManager: UserManager) => {
   server.route({
     method: "GET",
-    path: "/test",
-    handler: async (request, h, err) => {
-      return "test";
-    },
-  });
-
-  server.route({
-    method: "GET",
     path: "/users",
     handler: async (request, h, err) => {
       const users = await userManager.findMany();
@@ -33,16 +25,6 @@ export const register = async (server: Server, userManager: UserManager) => {
         email: randomInt(100) + "@test.com",
       };
       await userManager.createUser(createPayload);
-      return "Success!!";
-    },
-  });
-
-  server.route({
-    method: "POST",
-    path: "/users",
-    handler: async (request, h, err) => {
-      const createPayload = request.payload as CreateUserPayload;
-      // create using manager
       return "Success!!";
     },
   });
