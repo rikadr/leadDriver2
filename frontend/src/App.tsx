ab7fdc5d57af2926bfb2d9466314140c7f4f8b47
Rikard Dotzler <rikardnoer@gmail.com>
2023-10-15 12:12:34 +0200

    Frontend cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,19 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import { IApiResponse } from "./types";
-
+import React from "react";
 function App() {
-  const [message, setMessage] = useState("not set");
-  useEffect(() => {
-    fetch("/test")
-      .then((res) => res.json())
-      .then((data: IApiResponse<{ message: string }>) => {
-        if (data.data) {
-          console.log(data.data.message);
-        }
-      });
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Message from backend /test</p>
-        <p>
-          <i>{message ?? "Nothing :("}</i>
-        </p>
-      </header>
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        ''
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
--- frontend/src/login/login-api.ts
+++ frontend/src/login/login-api.ts
@@ -4,9 +4,9 @@ import { IApiResponse } from "../types";
 type LoginMutationResponse = IApiResponse<{ success: true }>;
 type LoginCredentials = { email: string; password: string };
 
-export const useLoginMutation = (credentials: LoginCredentials) => {
-  return useMutation<LoginMutationResponse, Error, string>({
-    mutationFn: () => {
+export const useLoginMutation = () => {
+  return useMutation<LoginMutationResponse, Error, LoginCredentials, unknown>({
+    mutationFn: (credentials: LoginCredentials) => {
       return fetch("/login", {
         method: "POST",
         // headers: { "Content-Type": "application/json" },
