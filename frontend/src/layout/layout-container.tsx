58055acdec0eaa5404b53b3146518db4f396baa4
Rikard Dotzler <rikardnoer@gmail.com>
2024-03-28 20:05:54 +0100

    Scrollbar styling

--- frontend/src/layout/layout-container.tsx
+++ frontend/src/layout/layout-container.tsx
@@ -4,7 +4,7 @@ export const LayoutContainer: React.FC<{ children: JSX.Element }> = (props) => {
   return (
     <div className="relative h-screen bg-zinc-900 font-display">
       <NavMenu />
-      <div className="py-4 px-6 h-full overflow-y-auto">
+      <div className="py-4 px-6 h-full overflow-y-auto scrollbar-margin">
         <div className="h-14" /> {/* Spacer to clear content below nav menu */}
         {props.children}
       </div>
