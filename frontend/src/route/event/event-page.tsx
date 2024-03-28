239db6717c5cc9a653306fb1642005ce1d9f305f
Rikard Dotzler <rikardnoer@gmail.com>
2024-03-28 23:29:09 +0100

    restyle event page

--- frontend/src/route/event/event-page.tsx
+++ frontend/src/route/event/event-page.tsx
@@ -2,7 +2,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useEvent } from "./event-api";
 import { AttendEvent } from "./attend-event/attend-event";
 import { CarCardLink } from "../cars/car-card";
-import { CardGridWrapper } from "../../components/card";
+import { Card, CardGridWrapper } from "../../components/card";
 import { Button } from "../../components/button";
 import { getAppUrl } from "../../utils/app-url";
 
@@ -18,8 +18,13 @@ export const EventPage: React.FC = () => {
   const { event, youAreAttending } = result;
   return (
     <div className="space-y-4">
-      <div className="flex items-baseline justify-between">
+      <div className="flex gap-4 items-baseline">
         <h1>{event?.name}</h1>
+        <p className="max-w-lg text-gray-500 italic">
+          {" "}
+          Owned by {event.owner.name}
+        </p>
+        <div className="grow" />
         {result.yourEvent && (
           <Button
             className="right-0 top-0"
@@ -29,22 +34,20 @@ export const EventPage: React.FC = () => {
           </Button>
         )}
       </div>
-      {event.location && (
-        <section>
-          <h3>Location</h3>
-          <p className="max-w-lg text-gray-500 italic">{event.location}</p>
-        </section>
-      )}
-      {event.description && (
-        <section>
-          <h3>Description</h3>
-          <p className="max-w-lg text-gray-500 italic">{event.description}</p>
-        </section>
-      )}
-      <section>
-        <h3>Event owner</h3>
-        <p className="max-w-lg text-gray-500 italic">{event.owner.name}</p>
-      </section>
+      <Card className="max-w-lg flex flex-col gap-2">
+        {event.location && (
+          <section className="flex items-baseline gap-1">
+            <h3>Location </h3>
+            <p className="max-w-lg text-gray-500 italic"> - {event.location}</p>
+          </section>
+        )}
+        {event.description && (
+          <section>
+            <h3>Description</h3>
+            <p className=" text-gray-500 italic">{event.description}</p>
+          </section>
+        )}
+      </Card>
       <AttendEvent event={event} youAreAttending={youAreAttending} />
       <p>Attendees: {event?.attendence?.length}</p>
       <CardGridWrapper>
