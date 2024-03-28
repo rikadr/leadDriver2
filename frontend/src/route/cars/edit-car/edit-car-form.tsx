7099583049247c5e803b34c0ed096b4867c9e592
Rikard Dotzler <rikardnoer@gmail.com>
2024-03-28 21:41:12 +0100

    Image preview

--- frontend/src/route/cars/edit-car/edit-car-form.tsx
+++ frontend/src/route/cars/edit-car/edit-car-form.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getAppUrl } from "../../../utils/app-url";
 import { CarDTO, EditCarPayload } from "shared";
 import { useNavigate } from "react-router-dom";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, set, useForm } from "react-hook-form";
 import { Button } from "../../../components/button";
 import { useDeleteCarMutation, useEditCarMutation } from "../cars-api";
 
@@ -16,6 +16,22 @@ export const EditCarForm: React.FC<{ car: CarDTO }> = ({ car }) => {
       imageUrl: car.imageUrl,
     },
   });
+  const imageUrlWatch = form.watch("imageUrl");
+
+  const [isValidImageUrl, setIsValidImageUrl] = useState(false);
+
+  useEffect(() => {
+    const image = new Image();
+    image.onloadstart = () => setIsValidImageUrl(false);
+    image.onload = () => setIsValidImageUrl(true);
+    image.onerror = () => setIsValidImageUrl(false);
+    const imageUrl = form.getValues("imageUrl");
+    if (imageUrl === "") {
+      setIsValidImageUrl(false);
+    } else if (imageUrl) {
+      image.src = imageUrl;
+    }
+  }, [form, imageUrlWatch]);
 
   const handleSubmit: SubmitHandler<EditCarPayload> = async (data) => {
     const result = await editCarMutation.mutateAsync({
@@ -61,6 +77,21 @@ export const EditCarForm: React.FC<{ car: CarDTO }> = ({ car }) => {
           Save
         </Button>
       </div>
+      <h2>Image preview</h2>
+      {isValidImageUrl && (
+        <img
+          src={form.getValues("imageUrl") || ""}
+          alt={car?.model}
+          className="max-h-96 w-full object-cover rounded-lg ring-[0.5px] ring-white/50"
+        />
+      )}
+      {!isValidImageUrl && (
+        <p className="rounded-lg ring-[0.5px] ring-white/50 p-4 h-32">
+          {" "}
+          {form.getValues("imageUrl") && "Invalid image url. "} Please provide a
+          url that links to an image
+        </p>
+      )}
     </form>
   );
 };
